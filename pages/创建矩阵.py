0fc003b281506d0e2e428c7160227b25936fc354
AndyLiu-art <1396019865@qq.com>
2023-01-19 18:31:26 +0800

    np

---
+++ pages/创建矩阵.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown("## 矩阵的创建和基本运算")
+st.sidebar.markdown("### 矩阵的简单计算")
---
+++ pages/矩阵的简单计算.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown("## 矩阵的创建和基本运算")
+st.sidebar.markdown("### 创建矩阵")
