68bc561573ad5b56dc9329b491ca146c63e273ab
AndyLiu-art <1396019865@qq.com>
2023-01-19 18:45:55 +0800

    np

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ st.sidebar.markdown("## Numpy库的学习")
 st.write("欢迎来到Numpy库学习的线上App，这里主打交互式学习体验")
 
 st.text("不信？你可以试试下面的操作喽！")
+
+st.write("Python是可以很方便地计算代数运算的，主要的支持库就是Numpy，它是Python最优秀的、基础的、实现数值计算的第三方库。")
