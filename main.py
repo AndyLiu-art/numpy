4ce200e29a3615a5a126bb6a7db5b5304644e75e
AndyLiu-art <1396019865@qq.com>
2023-01-19 18:40:49 +0800

    np

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.sidebar.markdown("## Numpy库的学习")
+
+st.write("欢迎来到Numpy库学习的线上App，这里主打交互式学习体验")
+
+st.text("不信？你可以试试下面的操作喽！")
--- pages/创建矩阵.py
+++ pages/创建矩阵.py
@@ -1,4 +1,18 @@
 import streamlit as st
 
 st.markdown("## 矩阵的创建和基本运算")
+
 st.sidebar.markdown("### 创建矩阵")
+
+st.sidebar.markdown("#### 创建向量")
+
+st.sidebar.markdown("##### 列表元组创建向量")
+st.sidebar.markdown("##### 特殊函数创建向量")
+
+st.sidebar.markdown("#### 向量重构数组")
+
+st.sidebar.markdown("#### 二维列表创建矩阵")
+
+st.sidebar.markdown("#### 特殊函数创建矩阵")
+
+st.sidebar.markdown("#### 单位阵")
